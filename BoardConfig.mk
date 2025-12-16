9a20129c215388f4af05905039f452f01cb86dba
xiaoxindada <2245062854@qq.com>
2025-12-16 16:31:58 +0800

    Clean duplicate defined f2fs ext4
    Signed-off-by: xiaoxindada <2245062854@qq.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -149,8 +149,6 @@ TARGET_RECOVERY_DEVICE_MODULES += \
     libpcrecpp \
     libxml2
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
 
 # Use mke2fs to create ext4 images
 TARGET_USES_MKE2FS := true
